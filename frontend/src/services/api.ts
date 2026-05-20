e169d809056e93af0751ff1618c6f8d8d8f98675
Rishu Raj <rishurajpnd18@gmail.com>
2026-05-20 21:28:54 +0530

    Fix API base URL configuration

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
-
+console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
 const api = axios.create({
-  baseURL: "https://supply-chain-ai-platform.onrender.com",
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "https://supply-chain-ai-platform.onrender.com/api",
 });
 
 export default api;
-
