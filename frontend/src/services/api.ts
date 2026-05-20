dea8f99754a6a08b65bf09341ab23a775eddbfde
Rishu Raj <rishurajpnd18@gmail.com>
2026-05-21 00:24:26 +0530

    cor fixed

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 console.log("API BASE URL ->:", import.meta.env.VITE_API_BASE_URL);
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  // baseURL: "http://127.0.0.1:8000/",
+  baseURL: "https://supply-chain-ai-platform.onrender.com/",
 });
 
 export default api;
